c0db0b7ce8e53fb71c779efb636795e48ecc1b8b
AV <Avastartis@gmail.com>
2023-02-15 18:18:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const ctx = canvas.getContext('2d');
 
 function init() {}
 init();
+
+const a = 2 * Math.PI / 6;
+const r = 50;
+
+
+function drawHexagon(x, y) {
+  ctx.beginPath();
+  for (var i = 0; i < 6; i++) {
+    ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
