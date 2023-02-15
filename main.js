16233abb330ddfc1a1257118623cd0d7db4973a2
AV <Avastartis@gmail.com>
2023-02-15 18:21:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,29 +4,22 @@ const ctx = canvas.getContext('2d');
 const a = 2 * Math.PI / 6;
 const r = 50;
 
-// 1st
-x = r;
-y = r;
-drawHexagon(x, y);
+function init() {
+  drawGrid(canvas.width, canvas.height);
+}
+init();
 
-// 2nd
-x = x + r + r * Math.cos(a);
-y = y + r * Math.sin(a);
-drawHexagon(x, y);
-
-// 3rd
-x = x + r + r * Math.cos(a);
-y = y - r * Math.sin(a);
-drawHexagon(x, y);
-
-// 4th
-x = x + r + r * Math.cos(a);
-y = y + r * Math.sin(a);
-drawHexagon(x, y);
+function drawGrid(width, height) {
+  for (let y = r; y + r * Math.sin(a) < height; y += r * Math.sin(a)) {
+    for (let x = r, j = 0; x + r * (1 + Math.cos(a)) < width; x += r * (1 + Math.cos(a)), y += (-1) ** j++ * r * Math.sin(a)) {
+      drawHexagon(x, y);
+    }
+  }
+}
 
 function drawHexagon(x, y) {
   ctx.beginPath();
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
   }
   ctx.closePath();
