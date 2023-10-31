f8b5d72917b6b890f82713410331c93c9d86034f
AV <Avastartis@gmail.com>
2023-10-31 14:33:14 -0400

    Update main.js

--- charts/main.js
+++ charts/main.js
@@ -1,3 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 function buildChart(title, xLabel, yLabel)
 {
     var ctx = document.getElementById('theChart');
