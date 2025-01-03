21fef2cac9cc5c30da6712e98ea32cc66279e78c
AV <Avastartis@gmail.com>
2025-01-03 16:28:52 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -62,7 +62,7 @@ function buildChart(title, xLabel, yLabel)
                     }
                 }]
             }
-          ,devicePixelRatio: 1
+          ,devicePixelRatio: 10
         },
         plugins: {
             afterUpdate: chart => {
